a9fa4401771135d28025e33b0747f30adb267987
Alexander Gilman <gilmanalex@gmail.com>
2016-12-05 01:08:58 -0800

    Added /www/lib items (ionic,leaflet,ngCordova) to bower.y

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,24 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope,$log, $ionicPlatform,$cordovaGeolocation) {
+    $scope.center = {
+      lat: 52.367215,
+      lng: 4.893036,
+      zoom: 1
+    };
+    $scope.maxbounds = {};
+    $scope.defaults = {
+      maxZoom: 16,
+      minZoom: 12,
+      tileLayer: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
+      tileLayerOptions: {
+          opacity: 0.9,
+          detectRetina: true,
+          reuseTiles: true,
+      },
+      scrollWheelZoom: true
+    };
+  
     $ionicPlatform.ready(function() {
         var posOptions = {timeout: 100000, maximumAge:100000, enableHighAccuracy: true};
         $cordovaGeolocation
